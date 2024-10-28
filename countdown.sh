fb4076dac9d8d5e6cb6c05113791fa504a628ff9
febrieluv  lala <febrinabilah124@gmail.com>
2024-10-28 07:25:43 +0000

    Save progress: 680.1

--- countdown.sh
+++ countdown.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-#Program that counts down to zero from a given argument
+
+# Program that counts down to zero from a given argument
 
 if [[ $1 -gt 0 ]]
 then
   for (( i = $1; i > 0; i-- ))
   do
-    echo $i sleep1
+    echo $i
+    sleep 1
   done
 else
   echo Include a positive integer as the first argument.
